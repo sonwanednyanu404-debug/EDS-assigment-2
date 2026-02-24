4d4fd3d600335ceb9a3aeac6909b98e528bccd5f
Dnyaneshwar Sonwane <sonwanednyanu404@gmail.com>
2026-02-24 22:28:33 +0530

    Add files via upload

---
+++ EDS 1.0.py
@@ -0,0 +1,45 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+==== RESTART: C:/Users/DELL/AppData/Local/Programs/Python/Python314/EDS 1.py ===
+Enter first number: 200
+Enter second number: 300
+Addition = 500.0
+>>> 
+==== RESTART: C:/Users/DELL/AppData/Local/Programs/Python/Python314/EDS 1.py ===
+Enter first number: 500
+Enter second number: 100
+Addition = 600.0
+Enter first number: 
+==== RESTART: C:/Users/DELL/AppData/Local/Programs/Python/Python314/EDS 1.py ===
+Enter first number: 1
+Enter second number: 1
+Subtraction = 0.0
+>>> 
+==== RESTART: C:/Users/DELL/AppData/Local/Programs/Python/Python314/EDS 1.py ===
+Enter first number: 500
+Enter second number: 100
+Subtraction = 400.0
+>>> 
+==== RESTART: C:/Users/DELL/AppData/Local/Programs/Python/Python314/EDS 1.py ===
+Enter first number: 50
+Enter second number: 5
+Multiplication = 250.0
+>>> 
+==== RESTART: C:/Users/DELL/AppData/Local/Programs/Python/Python314/EDS 1.py ===
+Enter first number: 500
+Enter second number: 2
+Division = 250.0
+>>> 
+==== RESTART: C:/Users/DELL/AppData/Local/Programs/Python/Python314/EDS 1.py ===
+Enter first number: 10
+Enter second number: 2
+Division = 5.0
+>>> 
+==== RESTART: C:/Users/DELL/AppData/Local/Programs/Python/Python314/EDS 1.py ===
+Enter temperature in Fahrenheit: 100
+Temperature in Celsius = 37.77777777777778
+>>> 
+==== RESTART: C:/Users/DELL/AppData/Local/Programs/Python/Python314/EDS 1.py ===
+Enter temperature in Celsius: 200
+Temperature in Fahrenheit = 392.0
---
+++ EDS 1.py
@@ -0,0 +1,32 @@
+'''a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+print("Addition =", a + b)'''
+
+'''a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+print("Subtraction =", a - b)'''
+
+'''a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+print("Multiplication =", a * b)'''
+
+'''a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+print("Division =", a / b)'''
+
+'''f = float(input("Enter temperature in Fahrenheit: "))
+
+c = (f - 32) * 5/9
+
+print("Temperature in Celsius =", c)'''
+
+c = float(input("Enter temperature in Celsius: "))
+
+f = (c * 9/5) + 32
+
+print("Temperature in Fahrenheit =", f)
+
